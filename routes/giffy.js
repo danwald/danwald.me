66444334254e525afd3d346c7c54bd6fab25722c
danny crasto <danwald79@gmail.com>
2019-07-23 07:39:16 +0400

    Updated dummy giffy.js app

--- routes/giffy.js
+++ routes/giffy.js
@@ -4,8 +4,6 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.writeHead(200, { 'Content-Type': 'text/json' });
     var obj = {
-        firstName: 'Dan',
-        lastName: 'Wald',
 		appName: 'Giffy',
     }
     res.end(JSON.stringify(obj));
