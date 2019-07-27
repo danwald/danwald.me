e0631adfe162c0914e06fe1ffd40d883670c8cde
danny crasto <danwald79@gmail.com>
2019-07-27 17:22:24 +0400

    getting images via multer and simple form

--- routes/giffy.js
+++ routes/giffy.js
@@ -7,9 +7,14 @@ router.get('/', function(req, res, next) {
 	res.render('giffy');
 });
 
-router.post('/', upload.single('photo'), (req, res) => {
-    if(req.file) {
-        res.json(req.file);
+router.post('/', upload.array('photos', 10), function (req, res, next) {
+	uploaded_files = req.files
+    if(uploaded_files) {
+		paths = []
+		uploaded_files.forEach(function(file){
+			paths.push(file['path'])
+		});
+	    res.json(paths.join(' '));
     }
     else throw 'error';
 });
