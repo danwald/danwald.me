addc8b8057d7100d8e11f98a92291a071f029a22
danny crasto <danwald79@gmail.com>
2019-08-03 10:31:52 -0700

    terser scaling

--- routes/giffy.js
+++ routes/giffy.js
@@ -8,7 +8,7 @@ const math = require('mathjs')
 const { spawn } = require('child_process');
 const multer = require('multer');
 const cmd = 'ffmpeg'
-const params = '-f image2 -framerate 30 -pattern_type glob -i %s -vf scale=%dx%d -f gif -'
+const params = '-f image2 -framerate 30 -pattern_type glob -i %s %s -f gif -'
 const max_extents = 600;
 const max_files = 50;
 const imageFolder = __dirname + '/uploads/images'
@@ -38,37 +38,35 @@ var getClientImagesGlob = function(id, filename) {
 
 router.post('/', upload.array(imageFormFieldName, max_files), function (req, res, next) {
     if(req.files) {
-        var genParams = ''
+        var scaleParam = ''
         calipers.measure(req.files[0].path, function (err, result) {
-          w = result.pages[0].width
-          h = result.pages[0].height
-          if (w > h && w > max_extents){
-              h = math.round(h/w * max_extents);
-              w = max_extents;
-          } else if(h > max_extents)  {
-              w = math.round(w/h * max_extents);
-              h = max_extents;
-          }
-		  genParams = util.format(params, getClientImagesGlob(req.body.clientId, req.files[0].path), w, h);
-		  subProc = spawn(cmd, genParams.split(' '));
-		  res.set('Content-Type', 'image/gif');
-		  subProc.stdout.pipe(res);
-		  subProc.on('error', (err) => {
-		    //console.log(`Failed to start subprocess. ${err}`);
+        w = result.pages[0].width
+        h = result.pages[0].height
+        if (w > h && w > max_extents){
+		    scaleParam = `-vf scale=${max_extents}:-1:flags=lanczos`;
+        } else if(h > max_extents)  {
+		    scaleParam = `-vf scale=-1:${max_extents}:flags=lanczos`;
+        }
+		genParams = util.format(params, getClientImagesGlob(req.body.clientId, req.files[0].path), scaleParam);
+		subProc = spawn(cmd, genParams.split(' '));
+		res.set('Content-Type', 'image/gif');
+		subProc.stdout.pipe(res);
+		subProc.on('error', (err) => {
+		  //console.log(`Failed to start subprocess. ${err}`);
+		});
+		subProc.stderr.on('data', (data) => {
+		  //console.log(`stderr: ${data}`);
+		});
+		subProc.on('close', (code) => {
+		  if (code !== 0) {
+			console.log(`process exited with code ${code}`);
+		  }
+		  req.files.forEach(function(filepath) {
+		  	fs.unlinkSync(filepath.path, (err) => {
+		  	console.log("Failed to delete"  + filepath);
+		      });
 		  });
-		  subProc.stderr.on('data', (data) => {
-		    //console.log(`stderr: ${data}`);
-		  });
-		  subProc.on('close', (code) => {
-		    if (code !== 0) {
-		  	console.log(`process exited with code ${code}`);
-		    }
-			req.files.forEach(function(filepath) {
-				fs.unlinkSync(filepath.path, (err) => {
-				console.log("Failed to delete"  + filepath);
-			    });
-			});
-		  });
+		});
         });
     }
     else throw 'error';
