1ee7b1a526db35d2ba0ca740f3b19d4468de3782
danny crasto <danwald79@gmail.com>
2022-06-23 21:27:19 +0400

    creating server start script

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,2 +1,19 @@
-ports: 22, 1194/udp 80, 443
-curl -0 https://raw.githubusercontent.com/chovy/node-startup/master/init.d/node-app
+apt-get update
+apt-get upgrade
+apt-get install nginx ufw openvpn certbot python3-certbot-apache
+
+ufw allow 22
+ufw allow 80
+ufw allow 443
+ufw allow 1022
+ufw allow 1194/udp
+ufw status
+
+echo "Copying over nginx"
+cp -v nginx-site.conf /etc/nginx/sites-available/danwald.me
+ln -s /etc/nginx/sites-available/danwald.me /etc/nginx/sites-enabled/danwald.me
+
+echo "Creating certs"
+certbot --nginx
+systemctl status certbot.timer
+certbot renew --dry-run
